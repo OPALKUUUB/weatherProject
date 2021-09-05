ad00124ffae40f3fab1a423b0479f988aaf1c744
OPLAKUUUB <makara.atipat@gmail.com>
2021-09-05 09:50:55 +0700

    add input for query follow user

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const https = require("https")
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=52a69f97abf1620ae463d69b589a307e&q=Pattaya&units=metric";
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res) {
+    // console.log(req.body.cityName);
+    const query = req.body.cityName;
+    const apiKey = "52a69f97abf1620ae463d69b589a307e";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apiKey;
 
     https.get(url, function (response) {
         console.log(response.statusCode);
@@ -22,7 +32,7 @@ app.get("/", function (req, res) {
             const icon = weatherData.weather[0].icon
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<p>The weather is curently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in "+ weatherData.name +" is " + temp + " degrees Celcius.</h1>")
+            res.write("<h1>The temperature in " + weatherData.name + " is " + temp + " degrees Celcius.</h1>")
             res.write("<img src=" + imageURL + ">");
             res.send();
         })
